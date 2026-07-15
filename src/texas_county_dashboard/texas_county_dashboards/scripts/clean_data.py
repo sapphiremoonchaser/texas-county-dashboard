e8e61e228c29f050def9b617e6aaa7545adf3422
violetdiamonds <violetdiamonds@protonmail.com>
2026-07-15 17:48:46 -0500

    setup

---
+++ src/texas_county_dashboard/texas_county_dashboards/scripts/clean_data.py
@@ -0,0 +1,7 @@
+"""
+Summary:
+    Rename variables
+    Convert data types
+    Compute derived metrics
+    Export a clean county dataset
+"""
---
+++ src/texas_county_dashboard/texas_county_dashboards/scripts/download_census.py
@@ -0,0 +1,8 @@
+"""
+Summary:
+    Download ACS data
+    Download Population estimates
+    Save raw CSVs
+    Cache API responses
+    Log the download date
+"""
