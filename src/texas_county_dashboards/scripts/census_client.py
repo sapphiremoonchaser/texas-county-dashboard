09b74aee267b0051374f494bfcc0f66daa799b0a
violetdiamonds <violetdiamonds@protonmail.com>
2026-07-20 14:38:42 -0500

    build county_profile downloader

--- src/texas_county_dashboards/scripts/census_client.py
+++ src/texas_county_dashboards/scripts/census_client.py
@@ -3,8 +3,10 @@ Downloads data from the Census API
 """
 import requests
 import pandas as pd
-from notebooks.data_exploration.census_api_exploration.census_api_exploration import \
-    response, df
+from dateutil.tz.win import valuestodict
+from numpy.ma.extras import column_stack
+
+from texas_county_dashboards.constants.census import ACS_COUNTY_PROFILE
 
 
 class CensusClient:
@@ -51,17 +53,26 @@ class CensusClient:
             timeout=30
         )
 
+        # Raise an exception if 404, 500, or 403 error returned
         response.raise_for_status()
 
+        # Census API returns json
         data = response.json()
 
         return pd.DataFrame(
-            data[1:],
-            columns=data[0],
+            data[1:], # skip first row of column names
+            columns=data[0], # first row becomes column names
         )
 
 
     def county_profile(self):
+        """
+        Download Census data and return a dataframe.
+
+        Returns
+            df: Pandas DataFrame with requested columns from census api
+        """
+        # Build list of variables to request
         variables = [
             "NAME",
             *ACS_COUNTY_PROFILE.values(),
@@ -70,7 +81,28 @@ class CensusClient:
         df = self._get(
             variables=variables,
             geography={
-                "for": "county",
-                "in": "state:48"
+                "for": "county:*", # All counties
+                "in": "state:48" # In Texas
             },
         )
+
+        # Rename the columns
+        rename_map = {
+            value: key
+            for key, value in ACS_COUNTY_PROFILE.items()
+        }
+
+        df = df.rename(columns=rename_map)
+
+        # Reorder the columns
+        columns = [
+            "state",
+            "county",
+            "NAME",
+            *ACS_COUNTY_PROFILE.keys()
+        ]
+
+        df = df[columns]
+
+        return df
+
