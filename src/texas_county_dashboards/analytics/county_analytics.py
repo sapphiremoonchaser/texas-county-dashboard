b41ebc2210c50de413202a0df246d74ed38bbc32
violetdiamonds <violetdiamonds@protonmail.com>
2026-07-20 17:35:58 -0500

    create helper functions for loading and merging data

--- src/texas_county_dashboards/analytics/county_analytics.py
+++ src/texas_county_dashboards/analytics/county_analytics.py
@@ -17,14 +17,72 @@ class CountyAnalytics:
 
         self.county_profile = None
         self.education_profile = None
-        self.emplete_profile = None
+        self.employment_profile = None
         self.df = None
 
 
-    # ToDo: load_data()
+    def _merge_data(self) -> pd.DataFrame:
+        """
+        Merge the following 3 dataframes:
+            county_profile
+            education_profile
+            employment_profile
+        :return: one merged dataframe
+        """
+        df = self.county_profile.copy()
+
+        # Merge county profile and education profile
+        df = df.merge(
+            self.education_profile,
+            on=[
+                "state",
+                "county",
+                "NAME"
+            ],
+            how="left"
+        )
+
+        # Merge above df with employment profile
+        df = df.merge(
+            self.employment_profile,
+            on=[
+                "state",
+                "county",
+                "NAME"
+            ],
+            how="left"
+        )
+
+        return df
 
 
-    # ToDo: _merge_data
+    def load_data(self) -> pd.DataFrame:
+        """
+        Load county_profile, education_profile, and employment_profile.
+        :return: one dataframe with merged data
+        """
+        # Load the county profile
+        self.county_profile = (
+            self.census_client
+            .county_profile()
+        )
+
+        # Load the education profile
+        self.education_profile = (
+            self.census_client
+            .education_profile()
+        )
+
+        # Load the employment profile
+        self.employment_profile = (
+            self.census_client
+            .employment_profile()
+        )
+
+        # Merge all of the data
+        self.df = self._merge_data()
+
+        return self.df
 
 
     # ToDo: calculate_metrics()
