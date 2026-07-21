2b9c0ec4bd7f7cf37255f1e9441c2b1161ab2a76
violetdiamonds <violetdiamonds@protonmail.com>
2026-07-21 14:22:19 -0500

    testing profile dataframe

--- src/texas_county_dashboards/analytics/county_analytics.py
+++ src/texas_county_dashboards/analytics/county_analytics.py
@@ -44,12 +44,12 @@ class CountyAnalytics:
         :return: one merged dataframe
         """
         profiles = [
-            self.county_profile,
-            self.education_profile,
-            self.employment_profile,
-            self.demographics_profile,
-            self.economics_profile,
-            self.housing_profile
+            self.county_profile
+            # self.education_profile,
+            # self.employment_profile,
+            # self.demographics_profile,
+            # self.economics_profile,
+            # self.housing_profile
         ]
 
         # Copy county profile
@@ -73,11 +73,11 @@ class CountyAnalytics:
 
         # Load census profiles
         self.county_profile = self.census_client.county_profile()
-        self.education_profile = self.census_client.education_profile()
-        self.employment_profile = self.census_client.employment_profile()
-        self.demographics_profile = self.census_client.demographics_profile()
-        self.economics_profile = self.census_client.economics_profile()
-        self.housing_profile = self.census_client.housing_profile()
+        # self.education_profile = self.census_client.education_profile()
+        # self.employment_profile = self.census_client.employment_profile()
+        # self.demographics_profile = self.census_client.demographics_profile()
+        # self.economics_profile = self.census_client.economics_profile()
+        # self.housing_profile = self.census_client.housing_profile()
 
         # Merge all of the data
         self.df = self._merge_data()
@@ -85,106 +85,106 @@ class CountyAnalytics:
         return self.df
 
 
-    def calculate_metrics(self) -> pd.DataFrame:
-        """
-        Create derived county metrics.
-            - bachelors_plus_pct
-            - unemployment_rate
-            - poverty_rate
-            - homeownership_rate
-            - vacancy_rate
-        :return: df including original and derived metrics
-        """
-
-        # Make sure the data is loaded
-        if self.df is None:
-            self.load_data()
-
-        # Calculate percent of people with a bachelor's degree or higher
-        self.df["bachelors_plus_pct"] = (
-            (
-                self.df["bachelors"]
-                + self.df["masters"]
-                + self.df["professional"]
-                + self.df["doctorate"]
-            )
-            /
-            self.df["population_25_plus"]
-            * 100
-        )
-
-        # Calculate unemployment rate
-        self.df["unemployment_rate"] = (
-            self.df["unemployed"]
-            /
-            self.df["labor_force"]
-            * 100
-        )
-
-        # Calculate poverty rate
-        self.df["poverty_rate"] = (
-            self.df["population_below_poverty"]
-            /
-            self.df["poverty_universe"]
-            * 100
-        )
-
-        # Calculate homeownership rate
-        self.df["homeownership_rate"] = (
-            self.df["owner_occupied_units"]
-            /
-            self.df["occupied_housing_units"]
-            * 100
-        )
-
-        # Calculate vacancy rate
-        self.df["vacancy_rate"] = (
-            self.df["vacanct_housing_units"]
-            /
-            self.df["housing_units"]
-            * 100
-        )
-
-        return self.df
-
-
-    def highest_income_counties(
-        self,
-        n=10
-    ) -> pd.DataFrame:
-        """
-        Sort counties by highest income.
-
-        :param n: top n counties
-        :return: top n counties dataframe
-        """
-        return (
-            self.df
-            .sort_values(
-                "median_household_income",
-                ascending=False
-            )
-            .head(n)
-        )
-
-
-    def largest_counties(
-        self,
-        n=10
-    ) -> pd.DataFrame:
-        """
-        Sort counties by largest population.
-
-        :param n: top n counties
-        :return: dataframe with top n largest counties by population
-        """
-        return (
-            self.df
-            .sort_values(
-                "population",
-                ascending=False
-            )
-            .head(n)
-        )
+    # def calculate_metrics(self) -> pd.DataFrame:
+    #     """
+    #     Create derived county metrics.
+    #         - bachelors_plus_pct
+    #         - unemployment_rate
+    #         - poverty_rate
+    #         - homeownership_rate
+    #         - vacancy_rate
+    #     :return: df including original and derived metrics
+    #     """
+    #
+    #     # Make sure the data is loaded
+    #     if self.df is None:
+    #         self.load_data()
+    #
+    #     # Calculate percent of people with a bachelor's degree or higher
+    #     self.df["bachelors_plus_pct"] = (
+    #         (
+    #             self.df["bachelors"]
+    #             + self.df["masters"]
+    #             + self.df["professional"]
+    #             + self.df["doctorate"]
+    #         )
+    #         /
+    #         self.df["population_25_plus"]
+    #         * 100
+    #     )
+    #
+    #     # Calculate unemployment rate
+    #     self.df["unemployment_rate"] = (
+    #         self.df["unemployed"]
+    #         /
+    #         self.df["labor_force"]
+    #         * 100
+    #     )
+    #
+    #     # Calculate poverty rate
+    #     self.df["poverty_rate"] = (
+    #         self.df["population_below_poverty"]
+    #         /
+    #         self.df["poverty_universe"]
+    #         * 100
+    #     )
+    #
+    #     # Calculate homeownership rate
+    #     self.df["homeownership_rate"] = (
+    #         self.df["owner_occupied_units"]
+    #         /
+    #         self.df["occupied_housing_units"]
+    #         * 100
+    #     )
+    #
+    #     # Calculate vacancy rate
+    #     self.df["vacancy_rate"] = (
+    #         self.df["vacanct_housing_units"]
+    #         /
+    #         self.df["housing_units"]
+    #         * 100
+    #     )
+    #
+    #     return self.df
+    #
+    #
+    # def highest_income_counties(
+    #     self,
+    #     n=10
+    # ) -> pd.DataFrame:
+    #     """
+    #     Sort counties by highest income.
+    #
+    #     :param n: top n counties
+    #     :return: top n counties dataframe
+    #     """
+    #     return (
+    #         self.df
+    #         .sort_values(
+    #             "median_household_income",
+    #             ascending=False
+    #         )
+    #         .head(n)
+    #     )
+    #
+    #
+    # def largest_counties(
+    #     self,
+    #     n=10
+    # ) -> pd.DataFrame:
+    #     """
+    #     Sort counties by largest population.
+    #
+    #     :param n: top n counties
+    #     :return: dataframe with top n largest counties by population
+    #     """
+    #     return (
+    #         self.df
+    #         .sort_values(
+    #             "population",
+    #             ascending=False
+    #         )
+    #         .head(n)
+    #     )
 
 
