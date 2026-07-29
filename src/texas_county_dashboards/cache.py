09cea56df71d2a01f80d814c92dceeafcdfc21c4
violetdiamonds <violetdiamonds@protonmail.com>
2026-07-29 15:07:06 -0500

    add data cache class

---
+++ src/texas_county_dashboards/cache.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import pandas as pd
+
+
+class DataCache:
+
+    def __init__(
+        self,
+        filename="data/processed/county_metrics.parquet"
+    ):
+        self.filename = Path(filename)
+
+
+    def save(
+        self,
+        df: pd.DataFrame
+    ):
+        self.filename.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
+
+        df.to_parquet(self.filename)
+
+
+    def load(self):
+        return pd.read_parquet(self.filename)
